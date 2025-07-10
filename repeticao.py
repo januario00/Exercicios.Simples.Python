d5c932487334a98d5009f07f3d6a923a9d4ff456
Daniele Januario <166251669+januario00@users.noreply.github.com>
2025-07-10 19:23:16 -0300

    Add files via upload
    Programa que recebe uma frase e a quantidade de vezes que o usuário deseja repeti-la, exibindo a frase na tela na quantidade especificada. Excelente para praticar entrada de dados, uso de loops e repetição em Python.

---
+++ repeticao.py
@@ -0,0 +1,6 @@
+frase = str(input("Digite uma frase :"))
+quantidade = int(input('Quantas vezes voce quer que essa frase seja repetida na tela? '))
+
+
+for i in range (quantidade):
+    print(frase)
